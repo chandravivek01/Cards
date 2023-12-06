b98fdb8ccd4cb126baf073ec5004bf1ced731275
Vivek Chandra Sardar <chandravivek52@gmail.com>
2023-12-06 09:05:17 +0530

    initial-commit...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  
+  posts = [
+    { title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'Saw this awesome tree during my hike today.'
+    },
+    { title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is the pic of a snowy mountain.'
+    },
+    { title: 'Mountain Biking',
+      imageUrl: 'assets/bike.jpeg',
+      username: 'bikes122',
+      content: 'I did some riding today.'
+    }
+  ];
 }
